36e9fa48d8660e028504a22c9232bcf1ff4ff549
sevim <sevimcaliskansc@gmail.com>
2019-04-19 11:12:28 +0200

    logging mistakes are fixed

--- model.py
+++ model.py
@@ -36,9 +36,8 @@ class ResNet_Train():
         # Detect if we have a GPU available
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self._Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.Tensor
-        self._Target = torch.cuda.LongTensor if torch.cuda.is_available() else torch.Tensor
         self._img = self._Tensor(self._opt.batch_size, 3, self._opt.image_size, self._opt.image_size)
-        self._cond = self._Target(self._opt.batch_size, 1)
+        self._cond = self._Tensor(self._opt.batch_size, 1)
         self._save_dir = os.path.join(self._opt.checkpoints_dir, self._opt.name)
         self._writer = SummaryWriter(self._save_dir)
 
@@ -47,6 +46,9 @@ class ResNet_Train():
         #scratch_optimizer = optim.SGD(scratch_model.parameters(), lr=0.001, momentum=0.9)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self._opt.lr,
                                              betas=[self._opt.adam_b1, self._opt.adam_b2])
+
+        if self._opt.load_epoch>=0:
+            self.load()
         self.criterion = nn.CrossEntropyLoss()
         model = self.train_model(self.model, self.dataloaders_dict, self.criterion, self.optimizer, num_epochs=30, is_inception=False)
         self._save_network(model, 31)
@@ -61,8 +63,8 @@ class ResNet_Train():
         best_model_wts = copy.deepcopy(model.state_dict())
         best_acc = 0.0
 
-        #number_iters_train = len(dataloaders['train'])/self._opt.batch_size
-        #number_iters_val = len(dataloaders['val'])/self._opt.batch_size
+        number_iters_train = len(dataloaders['train'])/self._opt.batch_size
+        number_iters_val = len(dataloaders['val'])/self._opt.batch_size
 
         for epoch in range(num_epochs):
             print('Epoch {}/{}'.format(epoch, num_epochs - 1))
@@ -120,9 +122,9 @@ class ResNet_Train():
 
                     loss_dict = {'loss':batch_loss, 'acc':batch_corrects.double()/self._opt.batch_size}
                     if phase=='train':
-                        self.plot_scalars(loss_dict, (i_train_batch+1)*self._opt.batch_size, True)
+                        self.plot_scalars(loss_dict, i_train_batch + number_iters_train*epoch, True)
                     else:
-                        self.plot_scalars(loss_dict, (i_train_batch+1)*self._opt.batch_size, False)
+                        self.plot_scalars(loss_dict, i_train_batch + number_iters_val*epoch, False)
 
 
 
