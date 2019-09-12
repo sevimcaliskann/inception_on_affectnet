454b353ae42250d5dc4dfe99b3d16e5e1556078a
sevim <sevimcaliskansc@gmail.com>
2019-09-12 11:39:14 +0200

    retrieving moods, the scripts are changed

--- launch/run_retrieve.sh
+++ launch/run_retrieve.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 python retrieve.py \
---data_dir /srv/glusterfs/csevim/datasets/affectnet \
+--data_dir /srv/beegfs02/scratch/emotion_perception/data/csevim/datasets/affectnet \
 --train_images_folder cropped2 \
 --test_images_folder cropped2 \
---train_ids_file /srv/glusterfs/csevim/dataset_affectnet_analysis/train_small.csv \
---test_ids_file /srv/glusterfs/csevim/dataset_affectnet_analysis/test_mood.csv \
---affectnet_info_file /srv/glusterfs/csevim/datasets/affectnet/training.csv \
---name resnet50_5d \
+--train_ids_file /srv/beegfs02/scratch/emotion_perception/data/csevim/datasets/dataset_affectnet_analysis/train_small.csv \
+--test_ids_file /srv/beegfs02/scratch/emotion_perception/data/csevim/datasets/dataset_affectnet_analysis/test_mood.csv \
+--affectnet_info_file /srv/beegfs02/scratch/emotion_perception/data/csevim/datasets/affectnet/training.csv \
+--name resnet50_sept_2d \
 --batch_size 64 \
---checkpoints_dir /srv/glusterfs/csevim/datasets/emotione/checkpoints \
+--checkpoints_dir /srv/beegfs02/scratch/emotion_perception/data/csevim/datasets/emotione/checkpoints \
 --load_epoch -1 \
 --dataset_mode mood \
 --model resnet50
