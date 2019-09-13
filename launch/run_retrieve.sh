100858200563fd783cbe8118196405c46e8207fa
sevim <sevimcaliskansc@gmail.com>
2019-09-13 20:55:55 +0200

    wrong pickle file is given in scripting, it is fixed

--- launch/run_retrieve.sh
+++ launch/run_retrieve.sh
@@ -6,8 +6,8 @@ python retrieve.py \
 --test_images_folder cropped \
 --train_ids_file /srv/beegfs02/scratch/emotion_perception/data/csevim/affwild/aff_wild_annotations_bboxes_landmarks_new/train_affwild_images.csv \
 --test_ids_file /srv/beegfs02/scratch/emotion_perception/data/csevim/affwild/aff_wild_annotations_bboxes_landmarks_new/test_affwild_images.csv \
---affectnet_info_file /srv/beegfs02/scratch/emotion_perception/data/csevim/datasets/affectnet/training.csv \
---name resnet50_mood_emo_1000 \
+--affectnet_info_file /srv/beegfs02/scratch/emotion_perception/data/csevim/affwild/aff_wild_annotations_bboxes_landmarks_new/train_all.pkl \
+--name resnet50_norm_1000 \
 --batch_size 64 \
 --checkpoints_dir /srv/beegfs02/scratch/emotion_perception/data/csevim/datasets/emotione/checkpoints \
 --load_epoch -1 \
